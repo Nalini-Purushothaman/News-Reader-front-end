15ded7a12bf4e272e55389c19ccb2d7cc4777d16
nalini-purushothaman <pnalini90sri@gmail.com>
2024-04-29 16:50:21 -0700

    Login restricted features added

--- src/NewsReader.js
+++ src/NewsReader.js
@@ -87,7 +87,27 @@ export function NewsReader() {
     setQuery(selectedQuery);
   }
 
+  function currentUserMatches(user) {
+    if (currentUser) {
+      if (currentUser.user) {
+        if (currentUser.user === user) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
   function onFormSubmit(queryObject) {
+    if (currentUser === null) {
+      alert("Log in if you want to create new queries!");
+      return;
+    }
+    if (savedQueries.length >= 3 && currentUserMatches("guest")) {
+      alert(
+        "guest users cannot submit new queries once saved query count is 3 or greater!"
+      );
+      return;
+    }
     let newSavedQueries = [];
     newSavedQueries.push(queryObject);
     for (let query of savedQueries) {
@@ -136,6 +156,7 @@ export function NewsReader() {
           <div className="box">
             <span className="title">Query Form</span>
             <QueryForm
+              currentUser={currentUser}
               setFormObject={setQueryFormObject}
               formObject={queryFormObject}
               submitToParent={onFormSubmit}
