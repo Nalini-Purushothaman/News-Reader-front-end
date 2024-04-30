0bd398498a5ed905659a5dd2c8459b984085e8f7
nalini-purushothaman <pnalini90sri@gmail.com>
2024-04-30 11:44:11 -0700

    Articles complete title is shown

--- src/Articles.js
+++ src/Articles.js
@@ -1,11 +1,12 @@
 export function Articles(params) {
   let articles = params.data.articles ? params.data.articles : [];
+  console.log(articles);
   let queryName = params.query.queryName ? params.query.queryName : "na";
   let articleCount = params.data.totalResults ? params.data.totalResults : 0;
   let queryText = params.query.q ? params.query.q : "na";
   let queryLanguage = params.query.language ? params.query.language : "na";
   let pageSize = params.query.pageSize ? params.query.pageSize : 0;
-  let currentUser;
+  let currentUser = null;
   if (params.currentUser) {
     currentUser = params.currentUser.user ? params.currentUser.user : "na";
     console.log("current User:", currentUser.user);
@@ -36,10 +37,10 @@ export function Articles(params) {
               if (item.title === "[Removed]") {
                 return <li key={idx}>Was Removed</li>;
               }
-              let trimTitle = item.title.substring(0, 30);
+              //let trimTitle = item.title.substring(0, 30);
               return (
                 <li key={idx}>
-                  {trimTitle}
+                  {item.title}
                   <a href={item.url} target="_blank" rel="noreferrer">
                     &nbsp;Link
                   </a>
